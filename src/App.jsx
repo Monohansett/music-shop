99fa27064fa9fe4eb5cb1e459ad4c95aaa6c8060
Monohansett <Frostyvi92@gmail.com>
2018-10-31 16:49:14 +0200

    configure css loader for webpack & display data from db

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
-    
+
     state = {
-        counter: 0
+        counter: 0,
+        instruments: []
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:3012/instruments`)
+            .then(res => {
+                const instruments = res.data;
+                this.setState({
+                    instruments: instruments
+                })
+            })
+
     }
 
     render() {
         return (
             <div>
-                {this.state.counter}
+                <ul>
+                    {this.state.instruments.map( instr => <li>{instr.name}</li>)}
+                </ul>
             </div>
         )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -8,6 +9,14 @@ module.exports = {
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             use: ['babel-loader']
+          },
+          {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract(
+              {
+                fallback: 'style-loader',
+                use: ['css-loader']
+              })
           }
         ]
       },
@@ -20,7 +29,13 @@ module.exports = {
       filename: 'bundle.js'
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+          inject: false,
+          hash: true,
+          template: './src/index.html',
+          filename: 'index.html'
+        })
       ],
     devServer: {
       contentBase: './dist',
